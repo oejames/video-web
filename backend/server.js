c2b899f9fdc94c477a11de3126c0122dd49ff27c
Ore James <jamesore200@gmail.com>
2024-12-13 21:05:51 -0600

    Updated jobqueue file name

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const { spawn } = require('child_process');
 const path = require('path');
 const fs = require('fs');
 const cors = require('cors');
-const transcriptionQueue = require('jobQueue');
+const transcriptionQueue = require('./jobqueue');
 
 
 const app = express();
